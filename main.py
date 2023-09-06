a034e6c2ebdcf24a26f188a796cf9d60a1252a7d
AndreyWBS <you@example.com>
2023-09-06 16:47:52 -0300

    verificação

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from selenium.webdriver.common.keys import Keys
 
 import pandas as pd
 
-caminho_pla_numeros = ""
-caminho_pla_num_ENV = ""
-caminho_pla_num_ERRO = ""
+caminho_pla_numeros = r"C:\Users\Micro\Desktop\planilhasBot\verificarNumeros\verificarNum.xlsx"
+caminho_pla_num = r"C:\Users\Micro\Desktop\planilhasGeradasBOT\numerosParaMensagemCCK"
 
+ondeMandarNumeros = "+55 48 9696-1694"
 
 planilha_de_numeros_env = {
     "numero": [],
@@ -22,14 +22,14 @@ planilha_de_numeros_Erro = {
     "codigo": []
 }
 
-#iniciando o google
+# iniciando o google
 driver = webdriver.Chrome()
 driver.get('https://web.whatsapp.com/')
 wait = WebDriverWait(driver, 120)
 elemento = wait.until(
     EC.visibility_of_element_located(
         (By.CSS_SELECTOR, '#app > div > div > div._2Ts6i._3RGKj > header > div._3WByx')))
-time.sleep(10)
+
 
 
 def procurarElementocomSeletor(seletor, driver):
@@ -61,26 +61,27 @@ def escrever(seletor, txt, driver):
     driver.find_element(By.CSS_SELECTOR, seletor).send_keys(Keys.ENTER)
 
 
-
-def gerarPLanilahas(nomeWPP):
+def gerarPLanilahas(caminhopla):
     df_Planilha_com_ERRO = pd.DataFrame(planilha_de_numeros_Erro)
     df_Planilha_com_ENV = pd.DataFrame(planilha_de_numeros_env)
-    nome_env = f"{nomeWPP}numeros_env.xlsx"
-    nome_erro = f"{nomeWPP}numeros_erros.xlsx"
-    caminho_planilha = r"C:\Users\Micro\Desktop\planilhasGeradasBOT\acionamentosWPP\pla"
 
-    df_Planilha_com_ERRO.to_excel(caminho_planilha + nome_erro, index=False)
-    df_Planilha_com_ENV.to_excel(caminho_planilha + nome_env, index=False)
+    df_Planilha_com_ERRO.to_excel(caminhopla + r"\num_erro.xlsx", index=False)
+    df_Planilha_com_ENV.to_excel(caminhopla + r"\num_env.xlsx", index=False)
 
 
-def comecar(driver, caminho_pla_numeros,ondeMandarNumeros):
-    numeros_df = pd.read_excel(caminho_pla_numeros)
+def obterPrimeiraLinha(texto):
+    linhas = texto.split("\n")
+    return linhas[0]
+
+
+def comecar(driver, caminho_pla_numeros, ondeMandarNumeros):
+
     try:
+        numeros_df = pd.read_excel(caminho_pla_numeros)
 
         nomeCVS = procurarElementscomseletor("._21S-L .l7jjieqr", driver)
 
         for i, mensagem in enumerate(numeros_df['numero']):
-
 
             numeroTELL = numeros_df.loc[i, "numero"]
             codigo = numeros_df.loc[i, "codigo"]
@@ -95,5 +96,35 @@ def comecar(driver, caminho_pla_numeros,ondeMandarNumeros):
                     escrever("#main .iq0m558w", str(numeroTELL), driver)
                     conversa = procurarElementscomseletor("._1jHIY , .ooty25bp", driver)
 
-    except:
-        print("não deu")
+                    for linha in conversa:
+
+                        linhatxt = obterPrimeiraLinha(linha.text)
+
+                        if linhatxt == str(numeroTELL):
+                            print("esse numero é igual")
+
+                            clicar_seletor_elemento("span._11JPr.selectable-text.copyable-text > span > a", linha,
+                                                    driver)
+
+                            try:
+                                conversarCOM = procurarElementocomSeletor(".iWqod._1MZM5._2BNs3.nqtxkp62.btzf6ewn",
+                                                                          driver)
+                                if conversarCOM.get_attribute("aria-label") == "Conversar com ":
+                                    print("esse numero da para mandar")
+                                    planilha_de_numeros_env["numero"].append(str(numeroTELL))
+                                    planilha_de_numeros_env["codigo"].append(str(codigo))
+                                    gerarPLanilahas(caminho_pla_num)
+                                    break
+                            except:
+                                planilha_de_numeros_env["numero"].append(str(numeroTELL))
+                                planilha_de_numeros_env["codigo"].append(str(codigo))
+                                print("esse numero não da")
+                                gerarPLanilahas(caminho_pla_num)
+                                break
+
+                continue
+    except Exception as e:
+        print("o erro é : ", e)
+
+
+comecar(driver, caminho_pla_numeros, ondeMandarNumeros)
